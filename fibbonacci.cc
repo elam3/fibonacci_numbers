5aaff98ef9b5c126ce45aa4c40729eba8f9889a8
Edison L <elam3@mail.ccsf.edu>
2019-02-02 22:21:32 -0800

    added array with fibonacci sequence.  added text output of fibonacci sequence.

--- fibbonacci.cc
+++ fibbonacci.cc
@@ -1,10 +1,43 @@
 #include <iostream>
 using namespace std;
 
+
+
+
+
+void userInput(int&);
+
+
+
+
+
 int main() {
     int count;
+    userInput(count);
+
+    //fib sequence
+    int fib[count];
+    for (int i = 0; i < count; i++) {
+        if (i < 2)
+            fib[i] = i;
+        else
+            fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    //print sequence
+    cout << count << " numbers from the fibonacci sequence:\n"
+        << "[";
+    for (int i = 0; i < count; i++) {
+        cout << fib[i] << ( (i < count - 1) ? ", " : ", ...]\n");
+    }
+    return 0;
+}
+
+
+
+
+
+void userInput(int& count) {
     cout << "How many digits of the fibbonacci sequence would you like to request? ";
     cin >> count;
-    cout << "Count: " << count << endl;
-    return 0;
 }
