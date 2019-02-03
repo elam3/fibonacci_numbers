a2806fa5fa1d7149421a4460a7d39ca6f22bb131
Edison L <elam3@mail.ccsf.edu>
2019-02-03 00:23:57 -0800

    refactor main method to use functions

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 > Write a program to ask user to input a count. Generate a fibonacci sequence in an array and print it for the count entered by the user. Use a function to generate the fibbonnacci numbers in the array. Use appropriate error handling.
 Fibonacci Numbers : https://www.mathsisfun.com/numbers/fibonacci-sequence.html
+
+# Sample Output
+
+> $ ./a.out
+> How many digits of the fibbonacci sequence would you like to request? 10
+> 10 numbers from the fibonacci sequence:
+> [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...]
--- fibonacci.cc
+++ fibonacci.cc
@@ -1,12 +1,21 @@
 #include <iostream>
 using namespace std;
-const int MAX = 10003; //unsigned long long int = 2**64 ~= 18 446 744 073 709 551 616; src: http://www.cplusplus.com/doc/tutorial/variables/
+
+
+
+
+
+//unsigned long long int = 2**64 ~= 18,446,744,073,709,551,616
+//src: http://www.cplusplus.com/doc/tutorial/variables/
+const int MAX = 10003; //segmentation fault at 10004
 
 
 
 
 
 void userInput(int&);
+void fillFibonacciArray(unsigned long long int [], const int);
+void printFibonacciArray(unsigned long long int [], const int);
 
 
 
@@ -15,22 +24,9 @@ void userInput(int&);
 int main() {
     int count = 0;
     userInput(count);
-
-    //fib sequence
     unsigned long long int fib[count];
-    for (int i = 0; i < count; i++) {
-        if (i < 2)
-            fib[i] = i;
-        else
-            fib[i] = fib[i - 1] + fib[i - 2];
-    }
-
-    //print sequence
-    cout << count << " numbers from the fibonacci sequence:\n"
-        << "[";
-    for (int i = 0; i < count; i++) {
-        cout << fib[i] << ( (i < count - 1) ? ", " : ", ...]\n");
-    }
+    fillFibonacciArray(fib, count);
+    printFibonacciArray(fib, count);
     return 0;
 }
 
@@ -39,7 +35,12 @@ int main() {
 
 
 void userInput(int& count) {
-    cout << "How many digits of the fibbonacci sequence would you like to request? ";
+    cout << "The Fibonacci Sequence is the series of numbers:\n\t0,1,1,2,3,5, 8, 13, 21, 34 , ..."
+        << "\nThe next number is found by adding the sum of the two numbers before it.\n"
+        << "\n(Source: https://www.mathsisfun.com/numbers/fibonacci-sequence.html)\n" 
+        << endl;
+
+    cout << "How many digits of the fibbonacci sequence would you like to print? ";
     do {
         if (count < 0)
             cout << "Opps. Please no negative numbers.\nHow many numbers? ";
@@ -48,4 +49,29 @@ void userInput(int& count) {
 
         cin >> count;
     } while (count < 0 || count > MAX);
+}
+
+
+
+
+
+void fillFibonacciArray(unsigned long long fib[], const int count) {
+    for (int i = 0; i < count; i++) {
+        if (i < 2)
+            fib[i] = i;
+        else
+            fib[i] = fib[i - 1] + fib[i - 2];
+    }
+}
+
+
+
+
+
+void printFibonacciArray(unsigned long long int fib[], const int count) {
+    cout << endl << count << " numbers from the fibonacci sequence:\n"
+        << "[";
+    for (int i = 0; i < count; i++) {
+        cout << fib[i] << ( (i < count - 1) ? ", " : ", ...]\n");
+    }
 }
